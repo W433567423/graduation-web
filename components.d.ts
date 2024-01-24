a330e6e1dc5eeb165b312414ebdc90f45325fd81
tutu <t433567423@163.com>
2024-01-24 17:51:04 +0800

    feat: dayjs

--- components.d.ts
+++ components.d.ts
@@ -13,14 +13,8 @@ declare module 'vue' {
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
-    ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
-    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Tarbar: typeof import('./src/components/tarbar.vue')['default']
-    TarBar: typeof import('./src/components/TarBar.vue')['default']
   }
 }
