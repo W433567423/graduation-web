5456c2bd1d0b0bd97d02d9f9a0cb23fd8aa217fe
tutu <t433567423@163.com>
2024-03-12 15:36:37 +0800

    fix: 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: './',
+	// base: './',
 	plugins: [
 		vue(),
 		AutoImport({
