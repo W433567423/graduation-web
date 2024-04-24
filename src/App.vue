d4b78b425fe55d2cb9404edb257c7f13a44dc831
tutu <t433567423@163.com>
2024-04-24 23:50:30 +0800

    feat:  判断pc端还是移动端

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
 import useUserStore from '@/stores/user.ts';
+import { isMobile } from '@/utils';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const userStore = useUserStore();
 userStore.init();
+onMounted(() => {
+	if (isMobile()) {
+		alert('手机端');
+		router.replace('/m_index');
+	} else {
+		alert('pc端');
+		router.replace('/pc_index');
+	}
+});
 </script>
 
 <template>
