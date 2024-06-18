d0e1f0856fe60faa34887230f7fd129ad1ba8d7e
tutu <t433567423@163.com>
2024-06-18 23:46:02 +0800

    fix: 修改提示

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <script lang="ts" setup>
-// import useUserStore from '@/stores/user.ts';
-// import { isMobile } from '@/utils';
-// import { onMounted } from 'vue';
-// import { useRouter } from 'vue-router';
+import { isMobile } from '@/utils';
+import { onMounted } from 'vue';
 
 // const router = useRouter();
 // const userStore = useUserStore();
 // userStore.init();
-// onMounted(() => {
-// 	if (isMobile()) {
-// 		console.log('手机端');
-// 		router.replace('/m-login');
-// 	} else {
-// 		console.log('pc端');
-// 		router.replace('/pc-login');
-// 	}
-// });
+onMounted(() => {
+	if (isMobile()) {
+		console.log('手机端');
+		// router.replace('/m-login');
+	} else {
+		console.log('pc端');
+		alert('禁止PC端访问，请使用手机端访问');
+		// router.replace('/pc-login');
+	}
+});
 </script>
 
 <template>
